8d8b7b9107d303d09b86e448a63853aa9586a015
Carter McCall <carmcca@siue.edu>
2022-01-05 14:22:24 -0600

    - implemented /marco API endpoint - send "Polo" as a reponse to GET requests on the marco endpoint

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ const express = require('express');
 
 const app = express();
 
+app.get("/marco", (req, res) => {
+    res.send("Polo");
+});
+
 app.listen(process.env.PORT || 3000, () => console.log("Server online"));
